d85ca4c49124ceab13a268a2857abbdb65851980
rafdavis <rd506269@gmail.com>
2025-05-05 12:29:18 -0300

    fix: Update userdata

--- user_data.sh
+++ user_data.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-sudo yum update -y
-
-sudo yum install ca-certificates wget amazon-efs-utils docker -y 
+sudo yum update -y && sudo yum install ca-certificates wget amazon-efs-utils docker -y 
 sudo systemctl enable docker && sudo systemctl start docker
 sudo usermod -aG docker ec2-user && newgrp docker
 
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose && sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-sudo mkdir -p /efs/data
-sudo mount -t efs -o tls fs-022baca0af7725ae9:/ /efs/data
+sudo mkdir -p /efs/data && sudo mount -t efs -o tls <DNS NAME>:/ /efs/data
 
 sudo wget -O /home/ec2-user/docker-compose.yml https://raw.githubusercontent.com/rafdavis/Projeto_AWS_Docker/refs/heads/main/docker-compose.yml
 sudo chown ec2-user:ec2-user /home/ec2-user/docker-compose.yml
 
-docker-compose up -d
+cd /home/ec2-user && docker-compose up -d
